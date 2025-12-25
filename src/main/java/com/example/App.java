220aed56ca7749ec470abe99c34a0451d33cfbfb
asmamahdi <asmamahdi267@gmail.com>
2025-12-24 19:27:00 -0500

    Refactor App.java to remove duplicate main method
    Removed duplicate main method and adjusted package declaration.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,15 +5,6 @@ public class App {
         System.out.println("Hello from Java CI/CD Pipeline!");
         System.out.println("This application was built by Jenkins!");
     }
-}
-
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java CI/CD Pipeline!");
-        System.out.println("This application was built by Jenkins!");
-    }
     
     // Method to add two numbers
     public static int add(int a, int b) {
